0e9bd3e1af22c882c7df08f4199237a6492fd21c
lorenzo <lorenzo.maddalena1@gmail.com>
2022-11-05 12:53:02 +0100

    test: added check squares testing

--- test_sudoku.py
+++ test_sudoku.py
@@ -20,6 +20,7 @@ def test_check_lines():
 
     assert sudoku1.check_rows() == True
     assert sudoku1.check_columns() == False
+    assert sudoku1.check_squares() == False
 
     sudoku2 = Sudoku(
         [
@@ -38,9 +39,17 @@ def test_check_lines():
 
     sudoku3 = Sudoku(
         [
-            [0],
-            [2],
-            [5]
-        ]
-    )
+            [2, 5, 0, 0, 3, 0, 9, 0, 1],
+            [0, 1, 0, 0, 0, 4, 0, 0, 0],
+            [4, 0, 7, 0, 0, 0, 2, 0, 8],
+            [0, 0, 5, 2, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 9, 8, 1, 0, 0],
+            [0, 4, 0, 0, 0, 3, 0, 0, 0],
+            [0, 0, 0, 3, 6, 0, 0, 7, 2],
+            [0, 7, 0, 0, 0, 0, 0, 0, 3],
+            [9, 0, 3, 0, 0, 0, 6, 0, 4]
+            ]
+        )
     assert sudoku3.check_columns() == True
+    assert sudoku3.check_rows() == True
+    assert sudoku3.check_squares() == True
