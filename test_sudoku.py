fcf6cc79d7cb133369a1829432cd38bcc971262b
lorenzo <lorenzo.maddalena1@gmail.com>
2022-11-05 12:32:11 +0100

    -test: tests functioning properly

--- test_sudoku.py
+++ test_sudoku.py
@@ -2,7 +2,7 @@ import pytest
 from sudoku_solver import Sudoku
 
 
-def check_testing():
+def test_check_lines():
 
     sudoku1 = Sudoku(
         [
@@ -20,3 +20,27 @@ def check_testing():
 
     assert sudoku1.check_rows() == True
     assert sudoku1.check_columns() == False
+
+    sudoku2 = Sudoku(
+        [
+            [1,0,3,0,5,0,7,0,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9]
+        ]
+    )
+    assert sudoku2.check_rows() == True
+
+    sudoku3 = Sudoku(
+        [
+            [0],
+            [2],
+            [5]
+        ]
+    )
+    assert sudoku3.check_columns() == True
